8c2cc9f2f690e6da5a943db9b20fb71acca8f825
Studium <Studium@DESKTOP-47EKLAR>
2021-01-26 10:30:47 +0100

    eso

---
+++ InnetPSP/src/Innet/HiloServidor.java
@@ -0,0 +1,94 @@
+package Innet;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Random;
+
+public class HiloServidor extends Thread
+{
+	DataInputStream fentrada;
+	Socket socket;
+	boolean fin = false;
+	DataOutputStream fsalida;
+	String[] cadena = {"Azul", "Amarillo", "Verde", "Rojo", "Marron"};
+	Random rng = new Random();
+
+	public HiloServidor(Socket socket)
+	{
+		this.socket = socket;
+		try
+		{
+			fentrada = new DataInputStream(socket.getInputStream());
+			fsalida = new DataOutputStream(socket.getOutputStream());
+		}
+		catch (IOException e)
+		{
+			System.out.println("Error de E/S");
+			e.printStackTrace();
+		}
+	}
+	// En el método run() lo primero que hacemos
+	// es enviar todos los mensajes actuales al cliente que se
+	// acaba de incorporar
+	public void run()
+	{
+		ChatServer.mensaje.setText("Número de conexiones actuales: " +
+				ChatServer.ACTUALES);
+		String texto = ChatServer.textarea.getText();
+		EnviarMensajes(texto);
+		// Seguidamente, se crea un bucle en el que se recibe lo que el cliente escribe en el chat.
+		// Cuando un cliente finaliza con el botón Salir, se envía un * al servidor del Chat,
+		// entonces se sale del bucle while, ya que termina el proceso del cliente,
+		// de esta manera se controlan las conexiones actuales
+		while(!fin)
+		{
+			String cadena = "";
+			try
+			{
+				cadena = fentrada.readUTF();
+				if(cadena.trim().equals("*"))
+				{
+					ChatServer.ACTUALES--;
+					ChatServer.mensaje.setText("Número de conexiones actuales: "
+							+ ChatServer.ACTUALES);
+					fin=true;
+				}
+				// El texto que el cliente escribe en el chat,
+				// se añade al textarea del servidor y se reenvía a todos los clientes
+				else
+				{
+					ChatServer.textarea.append(cadena + "\n");
+					texto = ChatServer.textarea.getText();
+					EnviarMensajes(texto);
+				}
+			}
+			catch (Exception ex)
+			{
+				ex.printStackTrace();
+				fin=true;
+			}
+		}
+	}
+	// El método EnviarMensajes() envía el texto del textarea a
+	// todos los sockets que están en la tabla de sockets,
+	// de esta forma todos ven la conversación.
+	// El programa abre un stream de salida para escribir el texto en el socket
+	private void EnviarMensajes(String texto)
+	{
+		for(int i=0; i<ChatServer.CONEXIONES; i++)
+		{
+			Socket socket = ChatServer.tabla[i];
+			try
+			{
+				DataOutputStream fsalida = new
+						DataOutputStream(socket.getOutputStream());
+				fsalida.writeUTF(texto);
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
+	}
+}
